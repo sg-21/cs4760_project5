104c08c79a637f2e46ca6aa031e6e3c9c76a8117
sg-21 <78775546+sg-21@users.noreply.github.com>
2021-05-03 01:28:09 -0500

    Update message.h

--- message.h
+++ message.h
@@ -1,1 +1,17 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+typedef struct {
+  long mtype;
+  int pid;
+  int address;
+  int action;
+} msg_t;
+
+enum actions {READ, WRITE, TERMINATE};
+
+/*mtype that oss will be looking for*/
+const int ossChannel = 20;
+
+#endif
 
