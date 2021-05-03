9e1b8e8f89f71bb747ba906ab977b99aef298f41
sg-21 <78775546+sg-21@users.noreply.github.com>
2021-05-03 01:27:33 -0500

    Update memManagement.h

--- memManagement.h
+++ memManagement.h
@@ -1,1 +1,25 @@
+#ifndef MEMORY_MANAGEMENT_H
+#define MEMORY_MANAGEMENT_H
 
+#define PROCESS_SIZE 32 
+#define TOTAL_MEMORY 256 
+
+/* Frame*/
+typedef struct {
+  unsigned ref : 8; /*reference bit*/
+  unsigned dirty : 1; /*dirty bit*/
+  int pid; /*process id of owner*/
+} frame_t;
+
+/* Page*/
+typedef struct {
+  int framePos; /*positio*/
+} page_t;
+
+/* Page Table*/
+typedef struct {
+  page_t pages[PROCESS_SIZE]; /*array of pages*/
+} pagetable_t;
+
+
+#endif
